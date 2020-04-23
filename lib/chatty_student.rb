c1edd404abb8d5e779722a9431a5f2c43c25c32f
chic-caller-7520 <arbrown110@gmail.com>
2020-04-23 02:11:16 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,13 @@
 class ChattyStudents
 
  def hello
-   puts "Hey there! I'm "
+   puts "Hey there! I'm so excited to learn stuff."
+ end
+ 
+ def raise_hand
+   puts "pick me"
+  end
+end  
+  
 
   
